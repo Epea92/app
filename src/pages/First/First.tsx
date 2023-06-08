bb23d256153f4f4e6437a748475daedafa8dcfde
Epea92 <javohirjumaev92@gmail.com>
2023-06-08 19:13:43 +0500

    upd

--- src/pages/First/First.tsx
+++ src/pages/First/First.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // @ts-ignore
 import style from './style.module.css'
+import io from '../../resources/io.png';
 function First(){
     return(
         <div className={style.container}>
@@ -15,7 +16,7 @@ function First(){
                 </div>
                 <div className={style.inputMenu}>
                     <div className={style.l1}></div>
-                    <div className={style.input}><input placeholder={'Search Anything'}/><img src={'https://s1.iconbird.com/ico/2013/9/452/w512h5121380476993params.png'}/></div>
+                    <div className={style.input}><input placeholder={'Search Anything'}/><img src={io}/></div>
                 </div>
             </div>
             <div className={style.secondPage}>
