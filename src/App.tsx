1b3c22d97361941df6635c9c0b3e0136c4644221
unknown <zavohirzumaev1@gmail.com>
2023-06-08 17:26:32 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import First from "./pages/First/First";
+
+function App() {
+  return (
+    <div className="App">
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/First/First.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// @ts-ignore
+import style from './style.module.css'
+function First(){
+    return(
+        <div className={style.container}>
+            <div className={style.header}>
+                <a className={style.TaskPro}>TaskPro</a>
+            </div>
+            <div className={style.elements}>
+                <div className={style.yellow}>
+                    <a className={style.w1}>Subscribe</a>
+                    <a className={style.w1}>To premium</a>
+                    <a className={style.w2}>Buy premium membership</a>
+                </div>
+                <div className={style.inputMenu}>
+                    <div className={style.l1}></div>
+                    <div className={style.input}><input placeholder={'Search Anything'}/></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default First;
