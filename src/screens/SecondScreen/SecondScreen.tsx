19a5de8f3a17b64c38828b7619ea87eb5e14e06b
Epea92 <javohirjumaev92@gmail.com>
2023-06-09 16:40:31 +0500

    upd

---
+++ src/screens/SecondScreen/SecondScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// @ts-ignore
+import style from './style.module.css'
+// @ts-ignore
+import back from '../../resources/back.png'
+
+function SecondScreen(){
+    return(
+        <div className={style.container}>
+            <div className={style.menu}>
+                <img className={style.back} src={back}/>
+            </div>
+        </div>
+    )
+}
