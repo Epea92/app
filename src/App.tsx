86793016eb98079c238cca196c29becab0d16d9a
Epea92 <javohirjumaev92@gmail.com>
2023-06-10 15:50:22 +0500

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Routes, Route } from "react-router-dom";
 import SecondScreen from "./screens/SecondScreen/SecondScreen";
 import First from "./pages/First/First";
 import Second from "./pages/Second/Second";
@@ -8,10 +7,6 @@ import Second from "./pages/Second/Second";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path = {'/'} element = {<App/>}/>
-        <Route path = {'/second-screen'} element = {<SecondScreen/>}/>
-      </Routes>
       <First/>
       <Second/>
       <SecondScreen/>
