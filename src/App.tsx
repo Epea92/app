be43d01b9fc634a07af6e3da75ac9fe353d657b2
Epea92 <javohirjumaev92@gmail.com>
2023-06-10 19:17:50 +0500

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import First from "./pages/First/First";
 import Second from "./pages/Second/Second";
-import AppDesignHeader from "./SecondPage/Header/AppDesignHeader";
-import William from "./SecondPage/William/William";
-import Info from "./SecondPage/Info/Info";
 
 
 function App() {
@@ -11,9 +8,6 @@ function App() {
     <div className="App">
       <First/>
       <Second/>
-        <AppDesignHeader/>
-        <Info/>
-        <William/>
     </div>
   );
 }
