d52c669ac32b7aa0fd23e2eca6337549ad4a752d
Epea92 <javohirjumaev92@gmail.com>
2023-06-12 15:12:46 +0500

    upd

--- src/screens/SecondScreen/WilliamDate/WilliamDate.tsx
+++ src/screens/SecondScreen/WilliamDate/WilliamDate.tsx
@@ -8,7 +8,11 @@ function WilliamDate(){
                 <img className={style.william} src={'https://lh3.googleusercontent.com/Hvu7FHODKN1AFv32g0x9zpedsUeQ49M1Zc4mOhsrvqhdbvcDTaGv_RxPfszk4AArMDOB3ZWK9Yef2bRRa7KBoUJPvzbY3SQjD4S_8FcPQA'}/>
                 <div className={style.text1}>
                     <a className={style.assigned}>Assigned to</a>
-                    <a className={style.name}>William Smith</a>
+                    <div className={style.name}>
+                        <a className={style.name1}>William</a>
+                        <a className={style.name2}>Smith</a>
+                    </div>
+
                 </div>
                 <div className={style.calendarInfo}>
                     <img className={style.calendar} src={'https://static.vecteezy.com/system/resources/previews/000/292/226/non_2x/business-calendar-vector-icon.jpg'}/>
@@ -18,7 +22,6 @@ function WilliamDate(){
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
