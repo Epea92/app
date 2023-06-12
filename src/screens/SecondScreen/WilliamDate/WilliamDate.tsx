72493f86e1a99c6f026f4128d785212415ebc0dd
Epea92 <javohirjumaev92@gmail.com>
2023-06-12 14:38:43 +0500

    upd

--- src/screens/SecondScreen/WilliamDate/WilliamDate.tsx
+++ src/screens/SecondScreen/WilliamDate/WilliamDate.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from './style.module.css';
 import william from '../../../resources/william.png'
+import calendar from '../../../resources/calendar.png'
 
 function WilliamDate(){
     return(
@@ -11,7 +12,15 @@ function WilliamDate(){
                     <a className={style.assigned}>Assigned to</a>
                     <a className={style.name}>William Smith</a>
                 </div>
+                <div className={style.calendar}>
+                    <img src={calendar}/>
+                    <div className={style.text}>
+                        <a className={style.date}>Due date</a>
+                        <a className={style.feb}>Feb 20</a>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
