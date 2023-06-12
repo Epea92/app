91b7a71bc184a211dfbd67d41be2965454815170
Epea92 <javohirjumaev92@gmail.com>
2023-06-12 13:39:06 +0500

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FirstPage from "./screens/FirstScreen/FirstPage/FirstPage";
 import SecondPage from "./screens/FirstScreen/SecondPage/SecondPage";
+import FirstPage1 from "./screens/SecondScreen/FirstPage/FirstPage1";
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
         <FirstPage/>
         <SecondPage/>
+        <FirstPage1/>
     </div>
   );
 }
---
+++ src/screens/SecondScreen/FirstPage/FirstPage1.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from './style.module.css';
+import back from '../../../resources/back.png'
+import settings from '../../../resources/settings.png';
+function FirstPage1(){
+    return(
+        <div className={style.container}>
+            <div className={style.header}>
+                <img className={style.back} src={back}/>
+                <a className={style.details}>Details</a>
+                <img className={style.settings} src={settings}/>
+            </div>
+        </div>
+    )
+}
+export default FirstPage1;
