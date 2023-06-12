dbfa8334f95e4a7c3c5a97256852457a41b338fd
Epea92 <javohirjumaev92@gmail.com>
2023-06-12 15:28:08 +0500

    upd

--- src/screens/SecondScreen/WilliamDate/WilliamDate.tsx
+++ src/screens/SecondScreen/WilliamDate/WilliamDate.tsx
@@ -5,15 +5,18 @@ function WilliamDate(){
     return(
         <div className={style.container}>
             <div className={style.info}>
-                <img className={style.william} src={'https://lh3.googleusercontent.com/Hvu7FHODKN1AFv32g0x9zpedsUeQ49M1Zc4mOhsrvqhdbvcDTaGv_RxPfszk4AArMDOB3ZWK9Yef2bRRa7KBoUJPvzbY3SQjD4S_8FcPQA'}/>
-                <div className={style.text1}>
-                    <a className={style.assigned}>Assigned to</a>
-                    <div className={style.name}>
-                        <a className={style.name1}>William</a>
-                        <a className={style.name2}>Smith</a>
-                    </div>
+                <div className={style.william}>
+                    <img className={style.williamPhoto} src={'https://lh3.googleusercontent.com/Hvu7FHODKN1AFv32g0x9zpedsUeQ49M1Zc4mOhsrvqhdbvcDTaGv_RxPfszk4AArMDOB3ZWK9Yef2bRRa7KBoUJPvzbY3SQjD4S_8FcPQA'}/>
+                    <div className={style.text1}>
+                        <a className={style.assigned}>Assigned to</a>
+                        <div className={style.name}>
+                            <a className={style.name1}>William</a>
+                            <a className={style.name2}>Smith</a>
+                        </div>
 
+                    </div>
                 </div>
+
                 <div className={style.calendarInfo}>
                     <img className={style.calendar} src={'https://static.vecteezy.com/system/resources/previews/000/292/226/non_2x/business-calendar-vector-icon.jpg'}/>
                     <div className={style.text2}>
