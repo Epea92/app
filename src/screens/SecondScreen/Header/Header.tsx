3a60451281eecf16374853a2239d2c046fbd331a
Epea92 <javohirjumaev92@gmail.com>
2023-06-13 16:25:42 +0500

    upd

--- src/screens/SecondScreen/Header/Header.tsx
+++ src/screens/SecondScreen/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import style from './style.module.css';
-import back from '../../../resources/back.png'
 import settings from '../../../resources/settings.png';
 import {Link} from "react-router-dom";
 function Header(){
@@ -9,7 +8,7 @@ function Header(){
             <div className={style.header}>
                 <div className={style.backBlock}>
                     <Link to={'/'}>
-                        <img className={style.back} src={back}/>
+                        <a className={style.back}>BACK</a>
                     </Link>
                 </div>
                 <a className={style.details}>Details</a>
