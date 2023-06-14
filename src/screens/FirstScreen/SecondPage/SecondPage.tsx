99f043b88743b62adade9841f09e01660c3a6312
Epea92 <javohirjumaev92@gmail.com>
2023-06-14 17:43:39 +0500

    upd

--- src/screens/FirstScreen/SecondPage/SecondPage.tsx
+++ src/screens/FirstScreen/SecondPage/SecondPage.tsx
@@ -1,52 +1,43 @@
 import React from "react";
 import style from './style.module.css';
 import phone from '../../../resources/phone.png'
-import hands from '../../../resources/hands.png';
 import laptop from '../../../resources/laptop.png'
-import dribble from '../../../resources/dribble.png'
-import {Link, Route, Routes} from "react-router-dom";
+import ball from '../../../resources/ball.png'
+import clients from '../../../resources/clients.png'
+import {Link} from "react-router-dom";
+
+
 function SecondPage(){
     return(
-        <div className={style.secondPage}>
-            <div className={style.dk2}>
-                <a className={style.pt}>
-                    Pending Task
-                </a>
-
-                <a className={style.sa}> See All</a>
+        <div className={style.container}>
+            <div className={style.title}><div className={'font-bold text-2xl'}>Pending Task</div><div className={'text-[#5fbc7f] font-bold'}>See All</div></div>
+            <div className={style.line}>
+                <div className={style.block1}>
+                    <Link to={'/app_design'}>
+                        <div className={style.block1_1}>
+                            <div className={style.inline}><img src={phone}/><div className={style.but}>47% Complete</div></div>
+                            <div className={style.text}>APP DESIGN</div>
+                            <div className={style.text2}>App Design Task</div>
+                        </div>
+                    </Link>
+                </div>
+                <div className={style.block2}>
+                    <div className={style.inline}><img src={laptop}/></div>
+                    <div className={style.text}>WEB</div>
+                    <div className={style.text2}>Web design</div>
+                </div>
             </div>
-
-
-
-            <div className={style.elements2}>
-                <div className={style.first}>
-                <Link to={'/app-design'}>
-                    <img src={phone}/>
-                    <a className={style.info}>
-                        APP DESIGN
-                    </a>
-                </Link>
+            <div className={style.line}>
+                <div className={style.block3}>
+                    <div className={style.inline}><img src={clients}/></div>
+                    <div className={style.text}>CLIENT</div>
+                    <div className={style.text2}>Ave studio des...</div>
                 </div>
-
-                <div className={style.second}>
-                    <img src={laptop}/>
-                    <a className={style.info}>WEB</a>
+                <div className={style.block4}>
+                    <div className={style.inline}><img src={ball}/></div>
+                    <div className={style.text}>DRIBBBLE</div>
+                    <div className={style.text2}>Create dribbble shot</div>
                 </div>
-
-
-            </div>
-            <div className={style.elements3}>
-                <div className={style.first1}>
-                    <img src={hands}/>
-                    <a className={style.info}>INFO</a>
-                </div>
-
-
-                <div className={style.second1}>
-                    <img src={dribble}/>
-                    <a className={style.info}>DRIBBBLE</a>
-                </div>
-
             </div>
         </div>
     )
